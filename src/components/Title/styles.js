e81d45b664d144a2957679dbc7c9f584f4ce6ef4
Igor <igor.torquato.20@gmail.com>
2024-06-04 20:29:16 -0300

    feat: add title

---
+++ src/components/Title/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+    color: #ffffff;
+    font-size: 28px;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 40px;
+`
